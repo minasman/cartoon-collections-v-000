028f33903ef99dd4c128ca80940776b9b8984705
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-19 18:08:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, i|
+    puts "#{i} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do |call|
+    
+  end
 end
 
 def long_planeteer_calls# code an argument here
