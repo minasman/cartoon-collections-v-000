a56a5f1f7667e7e38a59c1ff34959dc546ff36c1
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-19 18:14:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,12 @@ def summon_captain_planet(calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  long = false
+  calls.each do |call|
+    long = true if call.length > 4
+  end
+  return long
 end
 
 def find_the_cheese# code an argument here
